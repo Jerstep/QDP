f88f3762a095ea43caede997b517bb3ada889eeb
Unknown <jeremy.koskamp@live.nl>
2019-05-24 13:28:59 +0200

    [+] Stuff

---
+++ QuickPrototypeGame2/Assets/Scripts/MoveTowards.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MoveTowards
+{
+    public void MoveTowardsTargets(Vector3[] targets, Vector3 playerPosition, float moveSpeed)
+    {
+        for(int i = 0; i < targets.Length; i++)
+        {
+            float dist = Vector3.Distance(playerPosition, targets[i]);
+            for(float e = dist; e > 2f; e--)
+            {
+                playerPosition = Vector3.MoveTowards(playerPosition, targets[i], moveSpeed);
+            }
+        }
+    }
+}
---
+++ QuickPrototypeGame2/Assets/Scripts/TransformToVector3.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformToVector3
+{
+    public Vector3[] ConvertToVector3(Transform[] transforms)
+    {
+        Vector3[] converted = new Vector3[transforms.Length];
+        for(int i = 0; i < transforms.Length; i++)
+        {
+            converted[i] = transforms[i].position;
+        }
+        return converted;
+    }
+}
